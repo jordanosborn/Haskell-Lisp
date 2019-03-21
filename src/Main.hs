a69f4a38b3cb3279456ffe0905da06635c209e61
Jordan Osborn <jordanosborn0@gmail.com>
2019-03-21 20:12:18 +0000

    added repl

--- src/Main.hs
+++ src/Main.hs
@@ -2,10 +2,32 @@ module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 import Control.Monad.Except
+import System.IO
 import qualified Parser
 import Evaluator
 import Utilities.Types
 import Errors
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+    result <- prompt
+    if pred result then return ()
+    else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
 
 readExpr :: String -> ThrowsError LispVal
 readExpr input = case parse Parser.parseExpr "lisp" input of
@@ -15,5 +37,7 @@ readExpr input = case parse Parser.parseExpr "lisp" input of
 main :: IO ()
 main = do
     args <- getArgs
-    evaled <- return $ liftM show $ readExpr (head args) >>= eval
-    putStrLn $ extractValue $ trapError evaled
+    case length args of
+        0 -> runRepl
+        1 -> evalAndPrint $ head args
+        _ -> putStrLn "Program takes only 0 or 1 argument"
