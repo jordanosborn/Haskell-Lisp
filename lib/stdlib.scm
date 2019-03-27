1ee9d1bdfabaeb5732404bdd637bacc71d660f01
Jordan Osborn <jordanosborn0@gmail.com>
2019-03-27 20:34:40 +0000

    added standard library file

---
+++ lib/stdlib.scm
@@ -0,0 +1,9 @@
+(define (not x) (if x #f #t))
+
+(define (null? obj) (if (eqv? obj '()) #t #f))
+
+(define (list . objs) objs)
+
+(define (id obj) obj)
+
+(define (flip func) (lambda (arg1 arg2) (func arg2 arg1)))
