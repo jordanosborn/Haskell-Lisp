a8156d8fa4435e244f41e9f88f2d2ca85f1ed04d
Jordan Osborn <jordanosborn0@gmail.com>
2019-03-27 20:27:45 +0000

    working

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,15 @@
 module Main where
-import System.Environment
-import Text.ParserCombinators.Parsec hiding (spaces)
-import Control.Monad.Except
-import System.IO
+import           System.Environment
+import           Text.ParserCombinators.Parsec
+import           Control.Monad.Except
+import           System.IO
 import qualified Parser
-import Evaluator
-import Utilities.Types
-import Errors
-import Data.IORef
-import Data.Maybe
+import           Evaluator
+import           Utilities.Types
+import           Errors
+import           Data.IORef
+import           Data.Maybe
+import           Control.Applicative
 
 flushStr :: String -> IO ()
 flushStr str = putStr str >> hFlush stdout
@@ -17,7 +18,8 @@ readPrompt :: String -> IO String
 readPrompt prompt = flushStr prompt >> getLine
 
 evalString :: Env -> String -> IO String
-evalString env expr = runIOThrows $ fmap show $ liftThrows (readExpr expr) >>= eval env
+evalString env expr =
+    runIOThrows $ fmap show $ liftThrows (readExpr expr) >>= eval env
 
 evalAndPrint :: Env -> String -> IO ()
 evalAndPrint env expr = evalString env expr >>= putStrLn
@@ -25,24 +27,24 @@ evalAndPrint env expr = evalString env expr >>= putStrLn
 until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
 until_ pred prompt action = do
     result <- prompt
-    if pred result then return ()
-    else action result >> until_ pred prompt action
+    if pred result
+        then return ()
+        else action result >> until_ pred prompt action
 
-runOne :: String -> IO ()
-runOne expr = primitiveBindings >>= flip evalAndPrint expr
+runOne :: [String] -> IO ()
+runOne args = do
+    env <- primitiveBindings
+        >>= flip bindVars [("args", List $ map String $ drop 1 args)]
+    runIOThrows (show <$> eval env (List [Atom "load", String (head args)]))
+        >>= hPutStrLn stderr
 
 runRepl :: IO ()
-runRepl = primitiveBindings >>= until_ (== "quit") (readPrompt "Lisp> ") . evalAndPrint
-
-readExpr :: String -> ThrowsError LispVal
-readExpr input = case parse Parser.parseExpr "lisp" input of
-    Left err -> throwError $ Parser err
-    Right val -> return val
+runRepl =
+    primitiveBindings
+        >>= until_ (== "quit") (readPrompt "Lisp> ")
+        .   evalAndPrint
 
 main :: IO ()
 main = do
     args <- getArgs
-    case length args of
-        0 -> runRepl
-        1 -> runOne $ head args
-        _ -> putStrLn "Program takes only 0 or 1 argument"
+    if null args then runRepl else runOne args
--- src/Utilities/Types.hs
+++ src/Utilities/Types.hs
@@ -1,10 +1,12 @@
 module Utilities.Types where
-import Data.Complex
-import Data.Ratio
-import Data.Array
-import Data.IORef
-import Control.Monad.Except
-import Text.ParserCombinators.Parsec hiding (spaces)
+import           Data.Complex
+import qualified System.IO                     as IO
+import           Data.Ratio
+import           Data.Array
+import           Data.IORef
+import           Control.Monad.Except
+import           Text.ParserCombinators.Parsec
+                                         hiding ( spaces )
 
 type Env = IORef [(String, IORef LispVal)]
 type ThrowsError = Either LispError
@@ -32,27 +34,37 @@ data LispVal = Atom String
     | PrimitiveFunc ([LispVal] -> ThrowsError LispVal)
     | Func {params :: [String], vararg :: Maybe String,
         body :: [LispVal], closure :: Env}
+    | IOFunc ([LispVal] -> IOThrowsError LispVal)
+    | Port IO.Handle
 
 
 showVal :: LispVal -> String
 showVal val = case val of
     String contents -> "\"" ++ contents ++ "\""
-    Atom name -> name
+    Atom   name     -> name
     Number contents -> show contents
-    Bool True -> "#t"
-    Bool False -> "#f"
-    List contents -> "(" ++ unwordsList contents ++ ")"
-    DottedList head tail -> "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+    Bool   True     -> "#t"
+    Bool   False    -> "#f"
+    List   contents -> "(" ++ unwordsList contents ++ ")"
+    DottedList head tail ->
+        "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
     PrimitiveFunc _ -> "<primitive"
-    Func {params = args, vararg = varargs, body = body, closure = env} ->
-        "(lambda (" ++ unwords (map show args) ++
-            (case varargs of
-                Nothing -> ""
-                Just arg -> " . " ++ arg) ++ ") ...)"
+    Func { params = args, vararg = varargs, body = body, closure = env } ->
+        "(lambda ("
+            ++ unwords (map show args)
+            ++ (case varargs of
+                   Nothing  -> ""
+                   Just arg -> " . " ++ arg
+               )
+            ++ ") ...)"
+    Port   _ -> "<IO port>"
+    IOFunc _ -> "<IO primitive>"
 
 
-instance Show LispVal where show = showVal
-instance Eq LispVal where (==) = (==)
+instance Show LispVal where
+    show = showVal
+instance Eq LispVal where
+    (==) = (==)
 
 
 unwordsList :: [LispVal] -> String
